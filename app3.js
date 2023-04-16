a569030d441f952d0b6533121d387bc0741ff756
radheshyamgupta <radheshyamgupta008@gmail.com>
2023-04-16 22:41:26 +0530

    querySelector&&querySelectorAll

--- app3.js
+++ app3.js
@@ -1,6 +1,6 @@
-var head=document.getElementsByClassName("list-group-item")
-head[0].style.fontWeight="bold"
-head[1].style.fontWeight="bold"
-head[2].style.fontWeight="bold"
-head[3].style.fontWeight="bold"
-head[2].style.backgroundColor = "#00ff00";
+// var head=document.getElementsByClassName("list-group-item")
+// head[0].style.fontWeight="bold"
+// head[1].style.fontWeight="bold"
+// head[2].style.fontWeight="bold"
+// head[3].style.fontWeight="bold"
+// head[2].style.backgroundColor = "#00ff00";
