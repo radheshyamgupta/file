d00fe490a17aa9646b0e1233e46f7337003b84b1
radheshyamgupta <radheshyamgupta008@gmail.com>
2023-04-17 09:53:13 +0530

    inserted_ItemBefore_ItemLister

--- app2.js
+++ app2.js
@@ -1,4 +1,4 @@
-console.log(document.all[8].style.border = '1px solid black');
-var head=document.getElementsByClassName("list-group-item")
-head[2].textContent="Hello"
-head[2].style.fontWeight="bold"
+// console.log(document.all[8].style.border = '1px solid black');
+// var head=document.getElementsByClassName("list-group-item")
+// head[2].textContent="Hello"
+// head[2].style.fontWeight="bold"
---
+++ app5.js
@@ -0,0 +1,27 @@
+// var head1=document.querySelector("header")
+// console.log(head1)
+// head1.style.borderBottom="solid 4px #7fff00"
+// var input=document.querySelector('input')
+// input.value="notHello"
+// var items=document.querySelector(".list-group-item")
+// items.style.color="#ceff00"
+// var lastItems=document.querySelector(".list-group-item:last-child")
+// lastItems.style.color="#ceff00"
+
+var secondItem = document.querySelector(".list-group-item:nth-child(2)");
+secondItem.style.backgroundColor= "#00ff00";
+var thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+thirdItem.style.display = "none";
+//<!--querySelectorAll-->
+// var title=document.querySelectorAll(".title")
+// console.log(title)
+//     title[0].textContent="goodbuy"
+
+var odd = document.querySelectorAll("li:nth-child(odd)");
+for (var i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "green";
+}
+
+
+
+
