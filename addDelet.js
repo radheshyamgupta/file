f149156b76b8ca757ca22310a5f3fd44f57a89a5
radheshyamgupta <radheshyamgupta008@gmail.com>
2023-04-23 04:56:39 +0530

    AddedFilterFunctanality

--- addDelet.js
+++ addDelet.js
@@ -1,46 +1,99 @@
-var form = document.getElementById("addForm");
-var itemList = document.getElementById("items");
 
-form.addEventListener('submit', addItem);
-itemList.addEventListener("click", removeItem);
+addForm.addEventListener('submit', addItem);
 
 function addItem(e) {
-    e.preventDefault();
-   
-    var newItem = document.getElementById("item").value;
-    
-    // create new li element
-    var li = document.createElement("li");
-    li.className = "list-group-item";
-    
-    // add text node
-    var text = document.createTextNode(newItem);
-    li.appendChild(text);
-    
-    // add delete button
-    var deleteBtn = document.createElement("button");
-    deleteBtn.className = "btn btn-dark delet";
-    deleteBtn.appendChild(document.createTextNode('x'));
-    
-    // add span with float-right class
-    var span = document.createElement("span");
-    span.className = "float-right";
-    span.appendChild(deleteBtn);
-    li.appendChild(span);
-    
-    // add new item to list
-    itemList.appendChild(li);
-   
-   
+ e.preventDefault();
+
+  var itemName = document.getElementById('itemInput').value
+ 
+  var itemDescription = document.getElementById('description').value
+  
+
+  if (itemName === '') {
+    alert('Please enter an item name');
+    return;
+  }
+  if(itemDescription === '') {
+    alert('Please enter a description for the item');
+    return;
+  }
+
+  const itemList = document.getElementById('items');
+
+  const li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(itemName + ' - ' + itemDescription));
+
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-dark delet';
+  deleteBtn.appendChild(document.createTextNode('x'));
+
+  const editBtn = document.createElement('button');
+  editBtn.className = 'btn btn-dark delet edit';
+  editBtn.style.backgroundColor="#F0F8FF"
+  editBtn.style.color="blue"
+  editBtn.style.padding="10px"
+  editBtn.style.margin="5px"
+  editBtn.style.marginRight ="5px"
+  editBtn.appendChild(document.createTextNode('edit'));
+
+  const span = document.createElement('span');
+   span.className = 'float-right'
+  span.appendChild(editBtn);
+  span.appendChild(deleteBtn);
+  li.appendChild(span);
+
+  itemList.appendChild(li);
 }
-function removeItem(e) {
-    if (e.target.classList.contains("delet")) {
-        if (confirm('Are you sure?')) {
-            var li = e.target.parentElement.parentElement;
-            itemList.removeChild(li);
-        }
+
+const button = document.getElementById("searchBtn");
+const input = document.getElementById("filterButton");
+
+// Add an event listener to the button
+button.addEventListener("click", function()
+ {
+  console.log(1)
+   const inputValue = input.value.replace(/\s+/g, '').toLowerCase();
+
+
+  const itemList = document.getElementById('items');
+  for(var i=0;i<itemList.children.length;i++)
+  {
+     const itemText = itemList.children[i].textContent.replace(/\s+/g, '').toLowerCase();
+ 
+    if (itemText.includes(inputValue))
+{
+  itemList.children[i].style.display='block'
+}
+else{
+  itemList.children[i].style.display='none'
+}
+  }
+
+});
+
+
+
+
+const itemList = document.querySelector('#items');
+itemList.addEventListener('click', function(e) {
+  if (e.target.classList.contains('delet')) {
+    if (confirm('Are you sure?')) {
+      const li = e.target.parentElement.parentElement;
+      itemList.removeChild(li);
     }
-}
+  } else if (e.target.classList.contains('edit')) {
+    const li = e.target.parentElement.parentElement;
+    const itemName = li.querySelector('.list-group-item').textContent.split(' - ')[0];
+    const itemDescription = li.querySelector('.list-group-item-description').textContent;
+
+    document.querySelector('#item').value = itemName;
+    document.querySelector('#description').value = itemDescription;
+
+    itemList.removeChild(li);
+  }
+});
 
 
 
